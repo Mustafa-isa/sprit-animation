7d80ca3f71396bc9ddc198d4089153d6341191d1
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-01-28 16:11:08 +0200

    start sprot animation
    ..

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ let areaOf = canvas.getContext("2d")
 let widthOf =canvas.width
 let heightOf =canvas.height
 console.log(heightOf)
-console.log(widthtOf)
+console.log(widthOf)
+let playerGround =new Image()
+playerGround.src ="./sprit image.png"
+
+function some(){
+    areaOf.clearRect(0,0,widthOf,heightOf)
+    areaOf.fillRect(0,0,0,0)
+    
+}
+some()
 
 
-
-
-console.log(areaOf)
